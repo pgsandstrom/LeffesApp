bf34efa288c48c0f3da4f448465b65f352a3aa53
Per Sandstrom <pg.sandstrom@gmail.com>
2014-04-23 14:44:41 +0200

    removed circularism in slideshow

--- www/js/view.js
+++ www/js/view.js
@@ -1,6 +1,28 @@
 $(function () {
+    "use strict";
+
+    var innovation = window.innovation = window.innovation || {};
+    innovation.view = innovation.view || {};
+
+
+    innovation.view.updateButtons = function () {
+        if (currentIndex === 0) {
+            $('.prev').addClass('hide');
+        } else {
+            $('.prev').removeClass('hide');
+        }
+
+        if (currentIndex === itemCount - 1) {
+            $('.next').addClass('hide');
+        } else {
+            $('.next').removeClass('hide');
+        }
+    };
+
+
     var currentIndex = 0,
         itemCount = $('.carousel > li').length;
+    //TODO refresh itemCount
 
     /* add the active class to the first item to hide all the others */
     $('.carousel > li:eq(' + currentIndex + ')').addClass('active');
@@ -8,12 +30,9 @@ $(function () {
     $('.carousel-nav').on('click', function () {
         var $active = $('.carousel > li.active'),
             isNext = $(this).hasClass('next');
-        currentIndex = (currentIndex + (isNext ? 1 : -1)) % itemCount;
 
-        /* go back to the last item if we hit -1 */
-        if (currentIndex === -1) {
-            currentIndex = itemCount - 1;
-        }
+
+        currentIndex = (currentIndex + (isNext ? 1 : -1));
 
         var $next = $('.carousel > li:eq(' + currentIndex + ')');
         $active.addClass(isNext ? 'next-out' : 'prev-out');
@@ -30,7 +49,10 @@ $(function () {
             console.log("next animationend");
         });
 
+        innovation.view.updateButtons();
 
         return false;
     });
+
+    innovation.view.updateButtons();
 });
